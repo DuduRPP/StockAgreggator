9b0a3b2515ec95b020b3db4dd0b3bd4bce4e9d56
Eduardo Rambauske <dudurpp1@gmail.com>
2025-02-10 22:45:10 -0300

    feat: add base Spring project

---
+++ src/test/java/com/dudurpp/StockAggregator/StockAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudurpp.StockAggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
